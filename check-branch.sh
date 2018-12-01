c5758d99ca5daffdbf2992d5978c7115926a8a76
Camilo <camilo@ninjas.cl>
2018-12-01 16:45:50 -0300

    fixed github deploy script

--- check-branch.sh
+++ check-branch.sh
@@ -7,3 +7,9 @@ then
   echo "Commmit not on develop branch"
   exit -1
 fi
+
+if [ -z "${GH_TOKEN}" ]
+then
+  echo "Github Token not Found"
+  exit -1
+fi
--- deploy-github.sh
+++ deploy-github.sh
@@ -2,11 +2,27 @@
 
 set -e
 
-BOOK_DIR=${PWD}/_book
-cp -r ${BOOK_DIR}/* ./
-rm -rf ${BOOK_DIR}
+BOOK_DIR=$(pwd)
+mkdir ~/deploy
+cd ~/deploy
 
-git checkout master
+git clone -b master https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git .
+
+cp -r ${BOOK_DIR}/* ./
+cp -r ./_book/* ./
+rm -rf ./_book
+rm -rf *.Rmd
+rm -rf *.bib
+rm -rf *.Rproj
+rm -rf *.tex
+rm -rf *.yml
+rm -rf *.editorconfig
+rm -rf *.gitattributes
+rm -rf *.Rbuildignore
+rm -rf .Rproj.user
+rm -rf Rhistory
+rm -rf *.sh
+
 git add --all *
 git commit -m "deployed docs (travis build ${TRAVIS_BUILD_NUMBER})"
 git push -q -f origin master
