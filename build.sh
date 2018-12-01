3af47f5e985abe66ecb43b18200fc501df2e372b
Camilo <camilo@ninjas.cl>
2018-12-01 15:17:26 -0300

    fixed build docker script

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 # https://bookdown.org/yihui/bookdown/build-the-book.html
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 bookdown::render_book("index.Rmd", "bookdown::epub_book")
+
+# PDF Book gives some troubles because of gifs
+# bookdown::render_book("index.Rmd", "bookdown::pdf_book")
