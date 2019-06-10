21a05b56335acc495b846115b783360621450ea7
Camilo <camilo@ninjas.cl>
2019-06-09 21:42:12 -0400

    updated build command

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # https://bookdown.org/yihui/bookdown/build-the-book.html
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
-bookdown::render_book("index.Rmd", "bookdown::epub_book")
+
+# Epub output disabled due to github constraints
+# bookdown::render_book("index.Rmd", "bookdown::epub_book")
 
 # PDF Book gives some troubles because of gifs
 # bookdown::render_book("index.Rmd", "bookdown::pdf_book")
